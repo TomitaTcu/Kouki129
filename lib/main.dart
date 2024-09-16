3d0eb03334348d2cff2128513b88da725ea07ffc
TomitaTcu <kouki.tomita129@gmail.com>
2024-09-16 19:42:18 +0900

    change tittle2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GDSC Summer hackathon',
+      title: 'GDSC Summer hackathonnnn',
       theme: ThemeData(
         // This is the theme of your application.
         //
